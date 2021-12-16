9073826d5296924f5602e23ff1899f988d9cf408
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-12-16 11:26:13 +0900

    feat: スクレイピング部分完成

---
+++ data-scripts/scrape_books.py
@@ -0,0 +1,16 @@
+from scraping.aozora_bunko_scraper import AozoraBunkoScraper
+
+author_list_path = "author_list.txt"
+save_dir = "data"
+
+# load author_list
+with open(author_list_path, 'r', encoding='utf-8') as f:
+    author_list = f.readlines()
+    author_list = [s.split() for s in author_list]
+
+scraper = AozoraBunkoScraper(save_dir)
+
+for author_name in author_list:
+    scraper.get_theauthor_books(author_name)
+
+scraper.save()
